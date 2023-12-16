5584f81d41790afa3c4463eed7ae8ea52befaebb
adammetzinger <adamcm728@gmail.com>
2023-12-16 15:42:53 -0600

    saturday 12/16

--- js/main.js
+++ js/main.js
@@ -1,14 +1,20 @@
 /*----- constants -----*/
-
+const wordArray = ['fun', 'running', 'sound', 'aaaaaaa'];
 
 /*----- state variables -----*/
-let guessesleft =; 
+let guessesleft; 
+let wordguess;
 
 
 /*----- cached elements  -----*/
 
 
 /*----- event listeners -----*/
+document.querySelector('button').addEventListener('click', buildguess);
 
 
 /*----- functions -----*/
+
+function render() {
+    setupBoard();    
+}
