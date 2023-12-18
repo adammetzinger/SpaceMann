a74176111d78c3fcaf97cce8327acd574e45b2b0
adammetzinger <adamcm728@gmail.com>
2023-12-18 14:01:03 -0600

    update

--- js/main.js
+++ js/main.js
@@ -2,17 +2,30 @@
 const wordArray = ['fun', 'running', 'sound', 'aaaaaaa'];
 
 /*----- state variables -----*/
-let guessesleft, letterguess, playerguesses, secretWord;
+let guessesleft, playerguesses, secretWord;
 
 /*----- cached elements  -----*/
 const guess = document.querySelector('input');
 const message = document.querySelector('main > h1');
 const letters = document.getElementById('word');
+const letterguess = document.querySelector('input').value;
+const arrSecretWord = secretWord.split('');
 /*----- event listeners -----*/
-document.querySelector('button').addEventListener('click', buildguess);
-
+document.getElementById('wordbuild').addEventListener('click', buildguess);
+document.getElementById(guessselect).addEventListener('click', guessCheck);
 
 /*----- functions -----*/
+function guessCheck() {
+    secretWord.split('').forEach(char => {
+        if (letterguess.toLocaleLowerCase() === char) {
+
+        } else {
+            guessesleft - 1
+            
+        }
+    })
+}
+
 function chooseWord() {
     let i = Math.floor(Math.random() * wordArray.length);
     secretWord = wordArray[i];
@@ -36,10 +49,11 @@ function buildguess() {
     while (word.lastElementChild) {
         word.removeChild(word.lastElementChild);
       }
+    render();
 }
 
 function displaySpaceman() {
-
+    const spaceman = querySelector('main > div > img');
 }
 
 function setupBoard() {
