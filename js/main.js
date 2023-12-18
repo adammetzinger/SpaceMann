47f2587015efc331e8d77431e79c75ee7620ac0c
adammetzinger <adamcm728@gmail.com>
2023-12-18 12:20:39 -0600

    random word generator done

--- js/main.js
+++ js/main.js
@@ -2,22 +2,41 @@
 const wordArray = ['fun', 'running', 'sound', 'aaaaaaa'];
 
 /*----- state variables -----*/
-let guessesleft; 
-let wordguess;
-let playerguesses;
-
+let guessesleft, letterguess, playerguesses, secretWord;
 
 /*----- cached elements  -----*/
 const guess = document.querySelector('input');
-
+const message = document.querySelector('main > h1');
 /*----- event listeners -----*/
 document.querySelector('button').addEventListener('click', buildguess);
 
 
 /*----- functions -----*/
+function chooseWord() {
+    let i = Math.floor(Math.random() * wordArray.length);
+    return wordArray[i];
+}
+function rednerWord() {
+
+}
+function displaySpaceman() {
+
+}
 
 function render() {
     setupBoard();
     displayGuesses();
     displayMessage(); 
+}
+
+function setupBoard() {
+
+}
+
+function displayGuesses() {
+
+}
+
+function displayMessage() {
+
 }
