70299944fbb10805045a90a5fc118b7f62dd7968
adammetzinger <adamcm728@gmail.com>
2023-12-18 18:33:28 -0600

    end of day update

--- js/main.js
+++ js/main.js
@@ -9,10 +9,9 @@ const guess = document.querySelector('input');
 const message = document.querySelector('main > h1');
 const letters = document.getElementById('word');
 const letterguess = document.querySelector('input').value;
-const arrSecretWord = secretWord.split('');
 /*----- event listeners -----*/
 document.getElementById('wordbuild').addEventListener('click', buildguess);
-document.getElementById(guessselect).addEventListener('click', guessCheck);
+document.getElementById('guessselect').addEventListener('click', guessCheck(letterguess));
 
 /*----- functions -----*/
 // function guessCheck() {
@@ -30,13 +29,20 @@ function chooseWord() {
     let i = Math.floor(Math.random() * wordArray.length);
     secretWord = wordArray[i];
 }
+
 function rednerWord(secretWord) {
     for (let i =0; i < secretWord.length; i++) {
         const div = document.createElement('div');
-        div.innerHTML(secretWord[i]);
+        div.setAttribute('id', `${i}`)
         word.appendChild(div);
+        // const newdiv = document.getElementById(`${i}`);
+        // newdiv.innerHTML = secretWord[i];
+        // newdiv.style.fontSize = '80px';
+        // newdiv.style.color = 'white';
     }
 }
+
+render();
 
 function render() {
     // setupBoard();
@@ -49,8 +55,18 @@ function buildguess() {
     const word = document.getElementById('word');
     while (word.lastElementChild) {
         word.removeChild(word.lastElementChild);
-      }
+    }
     render();
+}
+
+function guessCheck(letterguess) {
+    for (let i = 0; i < secretWord.length; i++) {
+        if (secretWord[i] = letterguess) {
+            const newdiv = document.getElementById(`${i}`);
+            newdiv.innerHTML = secretWord[i];
+            newdiv.style.fontSize = '80px';
+        }
+    }
 }
 
 function displaySpaceman() {
