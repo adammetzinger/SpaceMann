628c39b3516c224176bc9848783456e1d6c32a7b
adammetzinger <adamcm728@gmail.com>
2023-12-20 12:44:50 -0600

    display message working

--- js/main.js
+++ js/main.js
@@ -1,30 +1,41 @@
 /*----- constants -----*/
+
 const wordArray = ['fun', 'running', 'sound', 'aaaaaaa'];
 
 // const spaceman = {
 //     '0': '',
-//     '1': '../images/1.png',
-//     '2': '../images/2.png',
-//     '3': '../images/3.png',
-//     '4': '../images/4.png',
-//     '5': '../images/5.png',
+//     '1': 'images/1.png',
+//     '2': 'images/2.png',
+//     '3': 'images/3.png',
+//     '4': 'images/4.png',
+//     '5': 'images/5.png',
 //     '6': new URL('../images/6.png')
 // }
 
 /*----- state variables -----*/
+
 let guessesleft, playerguesses, secretWord;
 
 /*----- cached elements  -----*/
+
 const guess = document.querySelector('input');
-const message = document.querySelector('main > h1');
+
+const messageEl = document.querySelector('main > h2');
+
 const letters = document.getElementById('word');
-const letterguess = document.querySelector('input').value;
+
+const correctLetters = [];
+
 const wrongletters = [];
 
+const wronglettersEl = document.getElementById('wrongletters');
+
+const spaceman = document.querySelector('main > div > img');
+
 /*----- event listeners -----*/
-document.getElementById('wordbuild').addEventListener('click', buildguess);
 
 document.querySelector('*').addEventListener('keypress', keyPressEvt);
+
 /*----- functions -----*/
 
 function chooseWord() {
@@ -45,23 +56,31 @@ init();
 function init() {
     chooseWord();
     rednerWord(secretWord);
-    guessesleft = 3;
-    displaySpaceman()
+    guessesleft = 6;
+    message();
 }
-function buildguess() {
+
+function buildGuess() {
     const word = document.getElementById('word');
     while (word.lastElementChild) {
         word.removeChild(word.lastElementChild);
     }
+    while (wrongletters.length > 0) {
+        wrongletters.pop();
+    }
+    wronglettersEl.innerHTML = ` ${wrongletters}`;
     init();
 }
 
+// gets the key thats pressed if undefined 
 function keyPressEvt(keyedin) {
     const secretWordArr = secretWord.split('');
     const letter = secretWordArr.find((element) => element === keyedin.key);
-    if (letter === undefined) {
+    if (correctLetters.length === secretWordArr.length || wrongletters.length === 6){
+        buildGuess();
+    } else if (letter === undefined) {
         wrongletters.push(keyedin.key);
-        document.getElementById('wrongletters').innerHTML = ` ${wrongletters}`;
+        wronglettersEl.innerHTML = ` ${wrongletters}`;
         guessesleft--;
         displaySpaceman()
     } else {
@@ -71,8 +90,20 @@ function keyPressEvt(keyedin) {
                 div.innerHTML = keyedin.key;
                 div.style.fontSize = '80px';
                 div.style.border = 'none';
+                correctLetters.push(keyedin.key);
             }
         }
+    }
+    message();
+}
+
+function message() {
+    if (correctLetters.length === secretWord.length) {
+        messageEl.innerHTML = 'You Won Congrats!!';
+    } else if (wrongletters.length === 6) {
+        messageEl.innerHTML = 'you lost womp womp';
+    } else {
+        messageEl.innerHTML = `You have ${guessesleft} guesses leftf`;
     }
 }
 
